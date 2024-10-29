aa89cc65b6e3f197c085edaa517b254e2ea09cbc
Yuki <30802523+haco28-Yuki@users.noreply.github.com>
2024-10-29 09:59:28 +0900

    Add files via upload

---
+++ website/blooming-belle/js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const zooming = new Zooming({
+    bgColor: 'rgb(0, 0, 0)',
+    bgOpacity: 0.8,
+  })
+
+  zooming.listen('.img-zoomable')
+})
