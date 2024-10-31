885ba56e77fecc8ba280e692fbcd6e18d182c193
Yuki <30802523+haco28-Yuki@users.noreply.github.com>
2024-10-31 14:16:18 +0900

    Add files via upload

--- website/blooming-belle/js/script.js
+++ website/blooming-belle/js/script.js
@@ -1,3 +1,23 @@
+// menu
+document.addEventListener('DOMContentLoaded', () => {
+  const menuButton = document.querySelector('.menu-btn');
+  const headerNavi = document.querySelector('.header-navi');
+
+  menuButton.addEventListener('click', () => {
+      headerNavi.classList.toggle('view');
+  });
+});
+
+// splide
+var splide = new Splide( '.splide', {
+  type   : 'loop',
+  padding: '3rem',
+  autoplay: 'true',
+} );
+
+splide.mount();
+
+// zooming
 document.addEventListener('DOMContentLoaded', function () {
   const zooming = new Zooming({
     bgColor: 'rgb(0, 0, 0)',
